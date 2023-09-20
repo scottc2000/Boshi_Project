af41297ccb0b0e356f55e0264d05f365f60feb6d
Ben Manner <benjaminmanner13@gmail.com>
2023-09-19 23:01:53 -0400

    Initial character and state machine classes and interfaces.

---
+++ Sprint0/Characters/Luigi.cs
@@ -0,0 +1,36 @@
+﻿using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Characters
+{
+    internal class Luigi : ICharcter
+    {
+        private LuigiStateMachine stateMachine;
+
+        public Luigi()
+        {
+            stateMachine = new LuigiStateMachine();
+        }
+
+        public void ChangeDirection()
+        {
+            stateMachine.ChangeDirection();
+        }
+        public void BeStomped()
+        {
+           stateMachine.BeStomped();
+        }
+        public void BeFlipped()
+        {
+            stateMachine.BeFlipped();
+        }
+        public void Update()
+        {
+
+        }
+    }
+}
