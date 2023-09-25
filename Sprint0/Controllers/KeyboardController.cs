b2a1957b77488c32b4adc900fe423db222d1bac0
Jerrin W <jwofford1110@gmail.com>
2023-09-25 01:37:02 -0400

    any last changes I made to try and get keyboard to work (did not work)

--- Sprint0/Controllers/KeyboardController.cs
+++ Sprint0/Controllers/KeyboardController.cs
@@ -13,15 +13,26 @@ namespace Sprint0.Controllers
     {
         public Dictionary<Keys, ICommand> controllerMappings;
 
+        private KeyboardState currentKeyState;
+        private KeyboardState previousKeyState;
+
         public KeyboardController()
         {
             controllerMappings = new Dictionary<Keys, ICommand>();
+            
         }
 
         public void RegisterCommand(Keys key, ICommand command)
         {
             controllerMappings.Add(key, command);
         }
+
+        private void getKeyState()
+        {
+            previousKeyState = currentKeyState;
+            currentKeyState = Keyboard.GetState();
+        }
+
         public void Update()
         {
             Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
