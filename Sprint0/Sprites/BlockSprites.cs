c67ae0906345a6c253eaaed881091fc478c84492
Michael Chen <chen.10917@buckeyemail.osu.edu>
2023-09-27 01:37:55 -0400

    Added Draw for BlockSprites

--- Sprint0/Sprites/BlockSprites.cs
+++ Sprint0/Sprites/BlockSprites.cs
@@ -7,34 +7,49 @@ using System.Text;
 using System.Threading.Tasks;
 using Sprint0.Interfaces;
 using Microsoft.Xna.Framework.Content;
+using System.Reflection.Emit;
 
 namespace Sprint0.Sprites
 {
     internal class BlockSprites : ISprite
     {
-        private readonly Texture2D _blocks;
+        private Sprint0 game;
+        private Texture2D _block;
+        
         private readonly int _columns = 74;
         private readonly int _rows = 21;
+        private Rectangle[] blockSprite;
 
-        public static Texture2D[,] blockLocations = new Texture2D[_columns, _rows];
+        private Vector2 position;
+        private Rectangle destination;
+        private float rotation = 0, layer = 0;
+        SpriteEffects effect = SpriteEffects.None;
 
-        public static void GetBlocks()
-        {
-            _blocks = Content.Load<Texture2D>("SpriteImages/blocks");
-            Rectangle sourceRect;
-            Color[] colors = new Color[32 * 32];
+        //private static Texture2D[,] blockLocations = new Texture2D[_columns, _rows];
 
-            for(int i = 0; i < _columns; i++)
-            {
-                for(int j = 0; j < _rows; j++)
-                {
+        //public static Texture2D[,] BlockLocations { get => blockLocations; set => blockLocations = value; }
 
-                }
-            }
-        }
+        //public static void GetBlocks()
+        //{
+        //    _blocks = Content.Load<Texture2D>("SpriteImages/blocks");
+        //    Rectangle sourceRect;
+        //    Color[] colors = new Color[32 * 32];
+
+        //    for(int i = 0; i < _columns; i++)
+        //    {
+        //        for(int j = 0; j < _rows; j++)
+        //        {
+
+        //        }
+        //    }
+        //}
 
         public BlockSprites() 
-        { 
+        {
+            
+            blockSprite = new Rectangle[] { new Rectangle(2076, 2, 32, 32) };
+            position.X = 600;
+            position.Y = 240;
             
         }
 
@@ -45,7 +60,11 @@ namespace Sprint0.Sprites
 
         public void Draw(SpriteBatch spriteBatch, ContentManager Content) 
         {
-            
+            _block = Content.Load<Texture2D>("SpriteImages/blocks");
+
+            destination = new Rectangle((int)position.X, (int)position.Y, 32, 32);
+
+            spriteBatch.Draw(_block, destination, blockSprite[0], Color.White, rotation, new Vector2(0, 0), effect, layer);
         }
     }
 }
