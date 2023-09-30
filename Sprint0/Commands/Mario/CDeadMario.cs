bfe116c9b471607d5755847154f5577bb4aa9e82
skansher <shergill.02.muskan@gmail.com>
2023-09-30 06:30:23 -0400

    commit dead-mario

---
+++ Sprint0/Commands/Mario/CDeadMario.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint0.Interfaces;
+using Sprint0.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Sprint0.Characters.Mario;
+
+namespace Sprint0.Commands.Mario
+{
+    internal class CDeadMario : ICommand
+    {
+        private Sprint0 mysprint;
+        private ICharacter mario;
+
+        public CDeadMario(Sprint0 mysprint)
+        {
+            this.mysprint = mysprint;
+        }
+
+        public void Execute()
+        {
+            mario = mysprint.mario;
+            mario.Die();
+        }
+    }
+}
