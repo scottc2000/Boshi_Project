4e78424faa72c7d76e978c40902e92011d08e2c9
skansher <117953654+skansher@users.noreply.github.com>
2023-09-30 07:45:22 -0400

    Update Sprint0.cs

--- Sprint0/Sprint0.cs
+++ Sprint0/Sprint0.cs
@@ -59,7 +59,7 @@ namespace Sprint0
             KeyboardController.RegisterCommand(Keys.D, new CMarioMoveRight(this));
             KeyboardController.RegisterCommand(Keys.E, new CMarioThrow(this));      // Still needs projectile
 
-            //KeyboardController.RegisterCommand(Keys.D5, new CDeadMario(this));
+            KeyboardController.RegisterCommand(Keys.Q, new CDeadMario(this));
             KeyboardController.RegisterCommand(Keys.D4, new CMarioRaccoon(this));
             KeyboardController.RegisterCommand(Keys.D3, new CMarioFire(this));
             KeyboardController.RegisterCommand(Keys.D2, new CMarioBig(this));
