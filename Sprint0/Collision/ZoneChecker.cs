49bea1da911aefc42aad4d8be35e46ad45d66665
Michael Chen <chen.10917@buckeyemail.osu.edu>
2023-10-19 18:31:28 -0400

    Updated zones to 256 pixels.

--- Sprint0/Collision/ZoneChecker.cs
+++ Sprint0/Collision/ZoneChecker.cs
@@ -21,36 +21,29 @@ namespace Sprint0.Collision
             int marioZone=0;
             switch (marioXPosition)
             {
-                case >= 0 and < 200:
+                case >= 0 and < 256:
                     marioZone = 1; break;
-                case <= 200 and < 400:
+                case <= 256 and < 512:
                     marioZone = 2; break;
-                case <= 400 and < 600:
+                case <= 512 and < 768:
                     marioZone = 3; break;
-                case <= 600 and < 800:
+                case <= 768 and < 1024:
                     marioZone = 4; break;
-                case <= 800 and < 1000:
+                case <= 1024 and < 1280:
                     marioZone = 5; break;
-                case <= 1000 and < 1200:
+                case <= 1280 and < 1536:
                     marioZone = 6; break;
-                case <= 1200 and < 1400:
+                case <= 1536 and < 1792:
                     marioZone = 7; break;
-                case <= 1400 and < 1600:
+                case <= 1792 and < 2048:
                     marioZone = 8; break;
-                case <= 1600 and < 1800:
+                case <= 2048 and < 2304:
                     marioZone = 9; break;
-                case <= 1800 and < 2000:
+                case <= 2304 and < 2560:
                     marioZone = 10; break;
-                case <= 2000 and < 2200:
+                case <= 2560 and < 2816:
                     marioZone = 11; break;
-                case <= 2200 and < 2400:
-                    marioZone = 12; break;
-                case <= 2400 and < 2600:
-                    marioZone = 13; break;
-                case <= 2600 and < 2800:
-                    marioZone = 14; break;
-                case <= 2800 and < 3000:
-                    marioZone = 15; break;
+                
             }
 
             return marioZone;
