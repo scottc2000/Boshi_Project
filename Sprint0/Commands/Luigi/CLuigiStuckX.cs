df085c98e86219596ea09818e916a841441a9cae
Ismail <51422343+ismail-dj@users.noreply.github.com>
2023-10-20 23:32:56 -0400

    Broke some stuff when pushing, should be fixed now

--- Sprint0/Commands/Luigi/CLuigiStuckX.cs
+++ Sprint0/Commands/Luigi/CLuigiStuckX.cs
@@ -1,32 +1,27 @@
-﻿using Microsoft.Xna.Framework;
-using Sprint0.Characters.MarioStates;
+﻿using System;
 using Sprint0.Interfaces;
-using Sprint0.Sprites;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Sprint0.Commands
+namespace Sprint0.Commands.Luigi
 {
-    internal class CLuigiStuckX : ICommand
-    {
-       
+	public class CLuigiStuckX : ICommand
+	{
+
         private Sprint0 mySprint0;
         private ICharacter luigi;
 
-        public CLuigiStuckX(Sprint0 Sprint0)
-        {
-            mySprint0 = Sprint0;
+        public CLuigiStuckX(Sprint0 mySprint0)
+		{
+            this.mySprint0 = mySprint0;
         }
+
         public void Execute()
         {
 
             luigi = mySprint0.objects.Players[1];
             luigi.lefthit = true;
-            
-            
+
+
         }
     }
 }
+
