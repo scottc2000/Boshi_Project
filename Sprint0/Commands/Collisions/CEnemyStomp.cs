8da266634808d0468c77ba150ac892df5bb6a21f
Scott <chen.10933@osu.edu>
2023-11-30 17:13:48 -0500

    Goomba collisions

---
+++ Sprint0/Commands/Collisions/CEnemyStomp.cs
@@ -0,0 +1,29 @@
+﻿using Sprint0.Characters;
+using Sprint0.Interfaces;
+using Microsoft.Xna.Framework;
+using Sprint0.Enemies;
+using Sprint0.GameMangager;
+
+namespace Sprint0.Commands.Collisions
+{
+    public class CEnemyStomp : ICommand
+    {
+        private Sprint0 sprint;
+        private IEnemies enemy;
+        private ObjectManager objectManager;
+
+        public CEnemyStomp(Sprint0 sprint, IEnemies enemy)
+        {
+            this.sprint = sprint;
+            this.enemy = enemy;
+            objectManager = sprint.objects;
+        }
+
+        public void Execute()
+        {
+            enemy.BeStomped();
+            objectManager.RemoveFromList(enemy);
+        }
+
+    }
+}
---
+++ Sprint0/Enemies/KoopaStates/StompedKoopaState.cs
@@ -0,0 +1,40 @@
+﻿using Sprint0.Enemies.GoombaStates;
+using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Enemies.GooombaStates
+{
+    public class StompedKoopaState : IEnemyState
+    {
+        private readonly Koopa koopa;
+
+        public StompedKoopaState(Goomba goomba)
+        {
+            this.koopa = koopa;
+        }
+
+        public void ChangeDirection()
+        {
+            
+        }
+
+        public void BeStomped()
+        {
+
+        }
+
+        public void BeFlipped()
+        {
+
+        }
+
+        public void Update()
+        {
+            koopa.currentSprite = koopa.mySpriteFactory.returnSprite("GoombaStomped");
+        }
+    }
+}
