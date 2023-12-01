f409626d803f2195f8a32f0d775a69faae3fc1fa
Ben Manner <144825439+BenjiManner@users.noreply.github.com>
2023-12-01 18:27:08 -0500

    revert before last push

--- Sprint0/obj/Debug/net6.0/Sprint0.AssemblyInfo.cs
+++ Sprint0/obj/Debug/net6.0/Sprint0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac71303b48cf36f959c3efba6614cd50c3c2a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
