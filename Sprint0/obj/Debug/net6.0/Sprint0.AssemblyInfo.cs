a81696c6f383cce61f2cec7f4a24a6a982b39f75
Ben Manner <144825439+BenjiManner@users.noreply.github.com>
2023-12-01 18:30:03 -0500

    Revert "New screen manager"

--- Sprint0/obj/Debug/net6.0/Sprint0.AssemblyInfo.cs
+++ Sprint0/obj/Debug/net6.0/Sprint0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb1d319d6f67edf0a97b829a4b7b6b04a4ad74c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
